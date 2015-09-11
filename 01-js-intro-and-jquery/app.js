c114e8e63bdf4110d5a1a4eb677dfebeec59a27e
Fer223 <lunabaratti@gmail.com>
2015-09-11 11:09:59 -0300

    [Fernando Luna Baratti]-1_1.1 to 1.6 - Added self closing tag,white line between selectors fixed,id convention

--- 01-js-intro-and-jquery/app.js
+++ 01-js-intro-and-jquery/app.js
@@ -1,5 +1,7 @@
 var main = function() {
+    
     $('.hidden').fadeIn(5000);
-    $( '.alias' ).focus().delay(5000); /*can't set the delay*/
+    //can't set delay
+    $( '.alias' ).focus().delay(5000); 
 };
 $(document).ready(main);
