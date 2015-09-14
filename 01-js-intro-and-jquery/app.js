283324cca904899536923a5c984c44007c33a847
Fer223 <lunabaratti@gmail.com>
2015-09-14 18:29:50 -0300

    [Fernando Luna Baratti]-1_1.1 to 1.7 - Fixed commented problems, working on exercise 11

--- 01-js-intro-and-jquery/app.js
+++ 01-js-intro-and-jquery/app.js
@@ -1,7 +1,27 @@
 var main = function() {
     
-    $('.hidden').fadeIn(5000);
-    //can't set delay
-    $( '.alias' ).focus().delay(5000); 
+    $('.hidden').fadeIn(5000, function() {
+
+        $( '.alias' ).focus()
+    });
+
+    $('#btn').click( function( event ) {
+
+        $.get("http://bootcamp.aws.af.cm/welcome/yourname", function(text) {
+            $('.hidden').html(text.response);
+        });
+    });
+
+    $('#btn2').click( function( event ) {
+
+       $.ajax({url: "https://api.spotify.com/v1/search",
+            type: 'get',
+            data: {q: "Rolling Stones", type: "album"},
+            function(data) { 
+                console.log(data);
+            }
+        });
+    });
 };
+
 $(document).ready(main);
