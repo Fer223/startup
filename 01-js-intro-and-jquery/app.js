ed28147230a67ce6a7f571b003781dc03fb153db
Fer223 <lunabaratti@gmail.com>
2015-09-16 01:25:31 -0300

    [Fernando Luna Baratti]-1_1.11 - Exercise 11 done

--- 01-js-intro-and-jquery/app.js
+++ 01-js-intro-and-jquery/app.js
@@ -1,27 +1,36 @@
-var main = function() {
+var onPageReady = function () {
     
-    $('.hidden').fadeIn(5000, function() {
-
-        $( '.alias' ).focus()
+    $('.hidden').fadeIn(5000, function () {
+        
+        $('.alias').focus();
     });
 
-    $('#btn').click( function( event ) {
-
-        $.get("http://bootcamp.aws.af.cm/welcome/yourname", function(text) {
-            $('.hidden').html(text.response);
+    $('.btn').on('click', function (event) {
+        
+        $.get('http://bootcamp.aws.af.cm/welcome/Fernando', function (data) {
+            
+            $('.hidden').html(data.response);
         });
     });
 
-    $('#btn2').click( function( event ) {
-
-       $.ajax({url: "https://api.spotify.com/v1/search",
-            type: 'get',
-            data: {q: "Rolling Stones", type: "album"},
-            function(data) { 
-                console.log(data);
+    $('.btn2').on('click', function (event) {
+        
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.spotify.com/v1/search',
+            data: {q: 'Rolling Stones', type: 'album'},
+            success: function (data) { 
+                
+                for (var i = 0; i < data.albums.items.length ; i++) {
+                    var albName = $('<p><b>Album</b>:'+data.albums.items[i].name+'</p>');
+                    var albImg = $('<img src="'+data.albums.items[i].images[0].url+'" />');
+                    var albUrl = $('<a href="'+data.albums.items[i].external_urls.spotify +'">In spotify</a>');
+                    var art = $('<article>').append(albName, albImg, albUrl);
+                    $('.articles-container').append(art);
+                };
             }
         });
     });
 };
 
-$(document).ready(main);
+$(document).ready(onPageReady);
